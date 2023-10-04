53ef64df99ce5ed37c9b12c24f491c53170c214b
Stefan Wegerhoff <1110820@uni-wuppertal.de>
2023-10-04 23:00:23 +0200

    add composer files

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
 //definiert Startseite
-
+require_once __DIR__ . '/vendor/autoload.php';
 define('CONFIG','config/config.ini');
 define('CONFIG_DEFAULT','config/default.ini');
 
