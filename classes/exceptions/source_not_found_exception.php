1c573938484073026d48dd2207d19271018c70c1
Stefan Wegerhoff <1110820@uni-wuppertal.de>
2025-02-19 15:01:26 +0100

    add classes/exceptions/source_not_found_exception.php

---
+++ classes/exceptions/source_not_found_exception.php
@@ -0,0 +1,18 @@
+<?php
+class SourceNotFoundException  extends  RuntimeException   {
+/* Eigenschaften */
+//protected int $severity ;
+/* Methoden */
+//public ErrorException::__construct ([ string $message [, int $code [, int $severity [, string $filename [, int $lineno ]]]]] )
+//final public int ErrorException::getSeverity ( void )
+/* Geerbte Methoden */
+//final public string Exception::getMessage ( void )
+//final public int Exception::getCode ( void )
+//final public string Exception::getFile ( void )
+//final public int Exception::getLine ( void )
+//final public array Exception::getTrace ( void )
+//final public string Exception::getTraceAsString ( void )
+//public string Exception::__toString ( void )
+//final private void Exception::__clone ( void )
+}
+?>
